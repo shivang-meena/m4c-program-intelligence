b17f7895f71ade8e73836214c450045d1eccebbc
shivang meena <shivang340908@gmail.com>
2026-06-23 14:37:39 +0530

    frontend deployment

--- frontend/src/services/api.tsx
+++ frontend/src/services/api.tsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Base instance
 const apiClient = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://m4c-program-intelligence.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
